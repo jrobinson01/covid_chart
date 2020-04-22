797e1b6d6aca1034061146d7c84a36402acb5fae
John Robinson <john.robinson@banno.com>
2020-04-22 09:53:52 -0400

    use new daily data source

--- src/components/cases-deaths-chart.js
+++ src/components/cases-deaths-chart.js
@@ -16,10 +16,13 @@ export default class CasesDeathsChart extends LitElement {
         type: Array,
       },
       selectedState: {
-        type: String,
+        type: Object,
       },
       selectedCounty: {
         type: String
+      },
+      statesData: {
+        type: Array,
       }
     }
   }
@@ -29,66 +32,126 @@ export default class CasesDeathsChart extends LitElement {
     this.chart = null;
   }
 
-  drawChart(counties, selectedState, selectedCounty) {
+  drawChart(counties, statesData, selectedState, selectedCounty) {
     console.log('drawChart', counties, selectedState, selectedCounty);
     if (!counties) {
       return;
     }
-    let filtered = [];
-    if(this.chart) {
-      this.chart.destroy();
-    }
+    let datasets = [];
+    let labels = [];
+    // if(this.chart) {
+    //   this.chart.destroy();
+    // }
     if (selectedState && !selectedCounty) {
-      filtered == counties.reduce((acc, val, index) => {
-        const previous = acc[acc.length-1];
-        if (!previous) {
-          // first
-          acc.push(Object.assign({}, val));
-          return acc;
-        } else {
-          if (previous.date === val.date) {
-            // same date, add countie's cases and deaths
-            previous.cases = parseInt(previous.cases) + parseInt(val.cases);
-            previous.deaths = parseInt(previous.deaths) + parseInt(val.deaths);
-          } else {
-            acc.push(Object.assign({}, val));
-          }
-          return acc;
-        }
-      }, []);
+      // TODO: use statesData instead...
+      // console.log('drawing state', selectedState, counties);
+      // filtered = counties.filter(c => c.state === selectedState).reduce((acc, val) => {
+      //   const previous = acc[acc.length-1];
+      //   if (!previous) {
+      //     // first
+      //     acc.push(Object.assign({}, val));
+      //     return acc;
+      //   } else {
+      //     if (previous.date === val.date) {
+      //       // same date, add county's cases and deaths
+      //       previous.cases = parseInt(previous.cases) + parseInt(val.cases);
+      //       previous.deaths = parseInt(previous.deaths) + parseInt(val.deaths);
+      //     } else {
+      //       acc.push(Object.assign({}, val));
+      //     }
+      //     return acc;
+      //   }
+      // }, []);
+      const filtered = statesData.filter(r => r.state === selectedState.abbreviation).reverse();
+      datasets = [
+        {
+          label: 'recovered',
+          borderColor: '#00FF00',
+          backgroundColor: '#00FF00',
+          data: filtered.map(f => f.recovered)
+        },
+        {
+          label: 'deaths',
+          borderColor: '#FF0000',
+          backgroundColor: '#FF0000',
+          data: filtered.map(f => f.death)
+        },
+        {
+          label: 'currently hospitalized',
+          borderColor: '#0000FF',
+          backgroundColor: '#0000FF',
+          data: filtered.map(f => f.hospitalizedCurrently)
+        },
+        {
+          label: 'positive tests',
+          borderColor: '#f7b577',
+          backgroundColor: '#f7b577',
+          data: filtered.map(f => f.positive)
+        },
+        {
+          label: 'negative tests',
+          borderColor: '#c6f777',
+          backgroundColor: '#c6f777',
+          data: filtered.map(f => f.negative)
+        },
+        {
+          label: 'curently in ICU',
+          borderColor: '#d9453b',
+          // backgroundColor: '#d9453b',
+          data: filtered.map(f => f.inIcuCurrently)
+        },
+        {
+          label: 'curently on Ventilator',
+          borderColor: '#bc3bd9',
+          // backgroundColor: '#bc3bd9',
+          data: filtered.map(f => f.onVentilatorCurrently)
+        },
+
+
+      ];
+      labels = filtered.map(f => f.date);
     } else if (selectedState && selectedCounty) {
-      filtered = counties.filter(r => r.state === selectedState && r.county === selectedCounty);
+      const filtered = counties.filter(r => r.state === selectedState.name && r.county === selectedCounty);
+      datasets = [
+        {
+          label: 'potential cases', // assuming every case infected 3 other people?
+          borderColor: '#0000FF',
+          data: filtered.map(f => parseInt(f.cases) * 3)
+        },
+        {
+          label: 'cases',
+          borderColor: '#CCCCCC',
+          data: filtered.map(f => f.cases)
+        },
+        {
+          label: 'deaths',
+          borderColor: '#FF0000',
+          data: filtered.map(f => f.deaths)
+        },
+      ];
+      labels = filtered.map(f => f.date);
     }
     const context = this.shadowRoot.querySelector('canvas').getContext('2d');
     console.log('canvas context', context);
-    const datasets = [
-      {
-        label: 'potential cases', // assuming every case infected 3 other people?
-        borderColor: '#0000FF',
-        data: filtered.map(f => parseInt(f.cases) * 3)
-      },
-      {
-        label: 'cases',
-        borderColor: '#CCCCCC',
-        data: filtered.map(f => f.cases)
-      },
-      {
-        label: 'deaths',
-        borderColor: '#FF0000',
-        data: filtered.map(f => f.deaths)
-      },
-    ];
-    this.chart = new Chart(context, {
-      type: 'line',
-      data: {
-        labels: filtered.map(f => f.date),
-        datasets
-      }
-    });
+
+    console.log('datasets', datasets);
+    if (!this.chart) {
+      this.chart = new Chart(context, {
+        type: 'line',
+        data: {
+          labels,
+          datasets
+        }
+      });
+    } else {
+      this.chart.data = {labels, datasets};
+      this.chart.update();
+    }
+
   }
 
   updated() {
-    this.drawChart(this.counties, this.selectedState, this.selectedCounty);
+    this.drawChart(this.counties, this.statesData, this.selectedState, this.selectedCounty);
     super.updated();
   }
 
--- src/covid-charts-app.js
+++ src/covid-charts-app.js
@@ -4,6 +4,59 @@ import './components/state-select.js';
 import './components/county-select.js';
 import './components/cases-deaths-chart.js';
 
+// map state names to abbreviations
+const ABBREVIATIONS = {
+  Alaska: 'AK',
+  Alabama: 'AL',
+  Arizona: 'AZ',
+  Arkansas: 'AR',
+  California: 'CA',
+  Colorado: 'CO',
+  Connecticut: 'CT',
+  Delaware: 'DE',
+  Florida: 'FL',
+  Georgia: 'GA',
+  Hawaii: 'HI',
+  Idaho: 'ID',
+  Illinois: 'IL',
+  Indiana: 'IN',
+  Iowa: 'IA',
+  Kansas: 'KS',
+  Kentucky: 'KY',
+  Louisiana: 'LA',
+  Maine: 'ME',
+  Maryland: 'MD',
+  Massachusetts: 'MA',
+  Michigan: 'MI',
+  Minnesota: 'MN',
+  Missouri: 'MO',
+  Montana: 'MT',
+  Nebraska: 'NE',
+  Nevada: 'NV',
+  'New Hampshire': 'NH',
+  'New Jersey': 'NJ',
+  'New York': 'NY',
+  'North Carolina': 'NC',
+  'North Dakota': 'ND',
+  Ohio: 'OH',
+  Oklahoma: 'OK',
+  Oregon: 'OR',
+  Pennsylvania: 'PA',
+  'Rhode Island': 'RI',
+  'South Carolina': 'SC',
+  'South Dakota': 'SD',
+  Tennessee: 'TN',
+  Texas: 'TX',
+  Utah: 'UT',
+  Vermont: 'VT',
+  Virginia: 'VA',
+  Washington: 'WA',
+  'West Virginia': 'WV',
+  Wisconsin: 'WI',
+  Wyoming: 'WY',
+  'District of Columbia': 'DC',
+}
+
 const STATES = {
   INITIAL: 'initial',
   LOADING_CSVS: 'loading-csvs',
@@ -16,7 +69,7 @@ const STATES = {
 function selectState(detail, context) {
   return {
     state: STATES.VIEW_BY_STATE,
-    context: {...context, ...{selectedState: detail.state, selectedCounty: undefined}}
+    context: {...context, ...{selectedState: {name: detail.state, abbreviation: ABBREVIATIONS[detail.state]}, selectedCounty: undefined}}
   }
 }
 
@@ -100,8 +153,11 @@ export default class CovidChartsApp extends LitElement {
   async loadCsvData() {
     const counties = await d3.csv('https://raw.githubusercontent.com/nytimes/covid-19-data/master/us-counties.csv');
     const pops = await d3.csv('data/populations-by-county-est2019.csv');
+    // only states but has way more data
+    const statesData = await d3.csv('https://covidtracking.com/api/v1/states/daily.csv');
+
     // precalculate unique state names
-    const states = counties
+    const stateNames = counties
     .map(r => r.state)
     .reduce((acc, val, index, self) => {
       if (self.indexOf(val) < index) {
@@ -120,7 +176,8 @@ export default class CovidChartsApp extends LitElement {
       }
       return acc;
     }, {});
-    this.appMachine('loaded', {counties, pops, states, stateCounties});
+
+    this.appMachine('loaded', {counties, pops, states:stateNames, stateCounties, statesData});
   }
 
   render() {
@@ -129,8 +186,8 @@ export default class CovidChartsApp extends LitElement {
     }
     return html`
       <state-select .states=${this.context.states} @selection=${event => this.onStateSelected(event.detail)}></state-select>
-      <county-select .counties=${ this.context.selectedState ? this.context.stateCounties[this.context.selectedState] : []} @selection=${event => this.onCountySelected(event.detail)}></county-select>
-      <cases-deaths-chart .counties=${this.context.counties} .selectedState=${this.context.selectedState} .selectedCounty=${this.context.selectedCounty}></cases-deaths-chart>
+      <county-select .counties=${ this.context.selectedState ? this.context.stateCounties[this.context.selectedState.name] : []} @selection=${event => this.onCountySelected(event.detail)}></county-select>
+      <cases-deaths-chart .statesData=${this.context.statesData} .counties=${this.context.counties} .selectedState=${this.context.selectedState} .selectedCounty=${this.context.selectedCounty}></cases-deaths-chart>
     `;
   }
 }
