7cd64775d1c42dbffa00de5ce874f5e23015cbee
John Robinson <john.robinson@banno.com>
2020-11-18 19:03:27 -0500

    disable tests vs population chart

--- src/components/state-view.js
+++ src/components/state-view.js
@@ -1,7 +1,6 @@
 import {LitElement, html, css} from 'lit-element';
 
 import './charts/deaths-by-county.js';
-import './charts/state-tests.js';
 import './charts/state-per-day.js';
 import './charts/state-county-cases.js';
 import './state-pop-info.js';
@@ -73,7 +72,6 @@ export default class StateView extends LitElement {
       .stateData=${this.currentStateData}>
       </state-pop-info>
       <state-per-day .stateData=${this.stateData} .selectedState=${this.selectedState}></state-per-day>
-      <state-tests .statePopData=${this.statePopData} .stateData=${this.currentStateData} .selectedState=${this.selectedState}></state-tests>
       <state-county-cases .countiesData=${this.countiesData} .selectedState=${this.selectedState}></state-county-cases>
       <deaths-by-county .countiesData=${this.countiesData} .selectedState=${this.selectedState}></deaths-by-county>
     </article>
